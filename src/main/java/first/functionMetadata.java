8afcd330badd4c42f43440f6a94f4531e71d760a
Olivier <45215922+obenichou@users.noreply.github.com>
2020-03-25 20:40:35 +0100

    Update functionMetadata.java

--- src/main/java/first/functionMetadata.java
+++ src/main/java/first/functionMetadata.java
@@ -3,17 +3,31 @@ package first;
 import java.util.List;
 import org.json.*;
 
+/**
+ * functionMetadata
+ * This class contain all metadata info possible of a function
+ */
 public class functionMetadata {
 	
-	String opcodes;
-	String architecture;
-	String name;
-	String prototype;
-	String comment;
-	List<String> apis;
-	String id;
-	JSONObject jsonObject;
-	
+	public String opcodes;
+	public String architecture;
+	public String name;
+	public String prototype;
+	public String comment;
+	public List<String> apis;
+	public String id;
+	public JSONObject jsonObject;
+
+	/**
+	 * Class Builder full
+	 * @param opcodes
+	 * @param architecture
+	 * @param name
+	 * @param prototype
+	 * @param comment
+	 * @param apis
+	 * @param id
+	 */
 	public functionMetadata(String opcodes, String architecture, String name, String prototype, String comment, List<String> apis, String id) {
 		this.opcodes=opcodes;
 		this.architecture=architecture;
@@ -23,14 +37,34 @@ public class functionMetadata {
 		this.apis=apis;
 		this.id=id;
 
+		this.jsonBuilder();
+	}
+
+	/**
+	 * Class Builder without the optional id
+	 * @param opcodes
+	 * @param architecture
+	 * @param name
+	 * @param prototype
+	 * @param comment
+	 * @param apis
+	 */
+	public functionMetadata(String opcodes, String architecture, String name, String prototype, String comment, List<String> apis) {
+	    this(opcodes, architecture, name, prototype, comment, apis, "");
+	}
+
+	/**
+	 * jsonBuilder create the needed JSON to simplify AddFunctionMetadata
+	 */
+	private void jsonBuilder(){
 		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("opcodes", opcodes);
-		jsonObject.put("architecture", architecture);
-		jsonObject.put("name", name);
-		jsonObject.put("prototype", prototype);
-		jsonObject.put("comment", comment);
-		jsonObject.put("apis", apis);
-		jsonObject.put("id", id);
+		jsonObject.put("opcodes", this.opcodes);
+		jsonObject.put("architecture", this.architecture);
+		jsonObject.put("name", this.name);
+		jsonObject.put("prototype", this.prototype);
+		jsonObject.put("comment", this.comment);
+		jsonObject.put("apis", this.apis);
+		jsonObject.put("id", this.id);
 
 		this.jsonObject = new JSONObject();
 		this.jsonObject.put("client_id", jsonObject);
