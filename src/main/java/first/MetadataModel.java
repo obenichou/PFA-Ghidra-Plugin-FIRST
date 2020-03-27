50644bdde2f6110c5ec35504ab58dc738ccc3e9d
Olivier <45215922+obenichou@users.noreply.github.com>
2020-03-27 07:11:32 +0100

    Update MetadataModel.java

--- src/main/java/first/MetadataModel.java
+++ src/main/java/first/MetadataModel.java
@@ -2,9 +2,21 @@ package first;
 
 import org.json.JSONObject;
 
+/**
+ * The type Metadata model.
+ */
 public class MetadataModel {
-	
-	public JSONObject jsonObject;	
+
+	/**
+	 * The Json object.
+	 */
+	public JSONObject jsonObject;
+
+	/**
+	 * Instantiates a new Metadata model.
+	 *
+	 * @param jsonObject the json object
+	 */
 	public MetadataModel (JSONObject jsonObject)
 	{
 		
