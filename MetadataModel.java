adb19dd986e8ed7eaa7d27a1e9eef463aa5509a4
saddam.cheikh <45017637+saddam-cheikh@users.noreply.github.com>
2020-03-27 05:29:10 +0100

    Add file

---
+++ MetadataModel.java
@@ -0,0 +1,13 @@
+package first;
+
+import org.json.JSONObject;
+
+public class MetadataModel {
+	
+	public JSONObject jsonObject;	
+	public MetadataModel (JSONObject jsonObject)
+	{
+		
+		this.jsonObject = jsonObject ;
+	}
+}
